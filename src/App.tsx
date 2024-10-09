1b166ea08a53febd78e3cec31f40fb90e3993c5f
berkantkazangirler <berkantkazangirler@gmail.com>
2024-10-09 17:15:09 +0300

    feat: created create post design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 function App() {
   return (
     <>
-      <p className="text-red-500">asda</p>
+      <div className="bg-main-bg w-full flex justify-center">
+        <div className="flex flex-col">
+          <div className="bg-element-bg w-128 rounded-xl">
+            <div className="flex flex-row gap-6 p-4">
+              <img
+                src={"./public/user.png"}
+                alt="User İmage"
+                className="rounded-full w-10"
+              />
+              <input
+                type="text"
+                placeholder="Let's share what going on your mind..."
+                className="w-full bg-input-bg placeholder:text-placeholder indent-3 text-white/70 rounded"
+              />
+              <button className="bg-button-bg text-white w-40 font-semibold rounded">
+                Create Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      'colors': {
+        'main-bg': '#1E252B',
+        'element-bg': '#262D34',
+        'input-bg': '#2C353D',
+        'placeholder': '#858EAD',
+        'button-bg': '#FF6934',
+      },
+      'width': {
+        '128': '49rem',
+      }
+    },
   },
   plugins: [],
 }
